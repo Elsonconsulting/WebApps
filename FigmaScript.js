6b00fda976fd20e7ea201354c5c4e1d161f453d3
Elsonconsulting <a.c.elson@bham.ac.uk>
2024-06-19 23:52:41 +0100

    Init

---
+++ FigmaScript.js
@@ -0,0 +1,11 @@
+javascript
+Copy code
+document.getElementById('clearBtn').addEventListener('click', function() {
+    document.getElementById('figmaForm').reset();
+});
+
+document.getElementById('figmaForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Form submitted!');
+    // You can add your form submission logic here
+});
